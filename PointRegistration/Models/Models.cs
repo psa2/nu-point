a79c70fa9bfdf300c26b9d5dcf2856bf249ec772
PawanKL <34525906+PawanKL@users.noreply.github.com>
2020-06-18 16:49:36 +0500

    add nu-point api

---
+++ PointRegistration/Models/Models.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PointRegistration.Models
+{
+    public class Driver
+    {
+        public string CNIC { get; set; }
+        public string DriverName { get; set; }
+        public string ContactNumber { get; set; }
+    }
+    public class Point
+    {
+        public int PointID { get; set; }
+        public string NumberPlate { get; set; }
+        public int NumberOfSeats { get; set; }
+        public string CNIC { get; set; }
+    }
+}
---
+++ PointRegistration/obj/Debug/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PointRegistration.WebApiApplication" Language="C#" %>
